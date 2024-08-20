86f4a34cb2b400448ddb2a8ba96476b4cd3976b1
jpearsall <a.bagel@gmail.com>
2024-08-20 12:55:45 -0600

    jwp - more terraform. idea cred issues after upgrading.

---
+++ tf/main.tf
@@ -0,0 +1,4 @@
+module "s3-bucket_jpcom_website" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+}
---
+++ tf/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "jpcom-terraform"
+    region         = "us-west-2"
+    key            = "state/jpcom-website/terraform.tfstate"
+    encrypt        = true
+    dynamodb_table = "jpcom-tf-state-lock"
+    profile        = "jpearsall"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
